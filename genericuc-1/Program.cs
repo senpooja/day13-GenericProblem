ac17ebf3b1c75206a8d6287d19446a01434f0d0a
senpooja <poojasen744055@gmail.com>
2022-08-04 14:21:27 +0530

    wellcome uc-1 file

---
+++ genericuc-1/Program.cs
@@ -0,0 +1,35 @@
+﻿class MaxNumCheck
+{
+    public static int MaxIntNumber(int first_value, int second_value, int third_value)
+    {
+
+        if (first_value.CompareTo(second_value) > 0 && first_value.CompareTo(third_value) > 0)
+
+        {
+            return first_value;
+        }
+        if (second_value.CompareTo(first_value) > 0 && second_value.CompareTo(third_value) > 0)
+
+        {
+            return second_value;
+        }
+        if (third_value.CompareTo(second_value) > 0 && third_value.CompareTo(first_value) > 0)
+
+        {
+            return third_value;
+        }
+        throw new Exception("first_value,second_value,Third_value are same");
+
+    }
+}
+
+class program
+{
+    static void Main(string[] args)
+    {
+
+        int output = MaxNumCheck.MaxIntNumber(11, 10, 39);
+        Console.WriteLine(output);
+
+    }
+}
